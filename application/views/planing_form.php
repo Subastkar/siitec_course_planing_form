009039095eac7f1f63ecbaa213fd92c7b8e3515f
Alberto Alcaraz <jaalcarazz@yahoo.com>
2012-04-04 12:28:24 -0500

    Css edited for h3 tag, planing form table added

--- application/views/planing_form.php
+++ application/views/planing_form.php
@@ -16,3 +16,42 @@
       <td><p>P&aacute;gina 1 de 3</p></td>
     </tr>
   </table>
+</div>
+
+<div id="content_form">
+  <span>
+    <h4 style="text-align: center;">Instituto Tecnol&oacute;gico de Colima</h4>
+    <h4 style="text-align: center;">Subdirecci&oacute;n acad&eacute;mica</h4>
+    <?php
+      echo '<h4 style="text-align: center;"> Departamento _____ </h4>';
+      echo '<h4 style="text-align: center;"> Planeaci&oacute;n del curso y avance program&aacute;tico del periodo _____ </h4>';
+    ?>
+  </span>
+
+  <span>
+    <p>Materia: _________________________________ HT ____ HP ____ CR ____ No. de Unidades _________</p>
+    <blockquote>
+      Objetivo de la matera:
+    </blockquote>
+    <p>Grupo: ____________ Carrera: _______________ Aula: ________________ Horario: _______________ Profesor: ______________________</p>
+  </span>
+
+  <table border ='1'>
+    <tr>
+      <th rowspan='2'>Unidad Tem&aacute;tica</th>
+      <th rowspan='2'>Subtemas</th>
+      <th colspan='2'>Fechas(Periodo)</th>
+      <th colspan='2'>Evaluaci&oacute;n</th>
+      <th rowspan='2'>Porcentaje de aprobaci&oacute;</th>
+      <th rowspan='2'>Firma del docente</th>
+      <th rowspan='2'>Firma del Jefe Acad&eacute;mico</th>
+      <th rowspan='2'>Observaciones</th>
+    </tr>
+    <tr>
+      <th>Programado</th>
+      <th>Real</th>
+      <th>Programada</th>
+      <th>Real</th>
+    </tr>
+  </table>
+</div>
