534757824851dceb3f9d2816350ff816baac3a7d
Alberto Alcaraz <jaalcarazz@yahoo.com>
2012-04-04 11:35:42 -0500

    html helper added, planing form head table finished

--- application/views/planing_form.php
+++ application/views/planing_form.php
@@ -1,18 +1,18 @@
 <div id="head_form">
   <table border = 1>
     <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td><p>Nombre del formato: Formato para la planeaci&oacute;n del Curso y Avance Program&aacute;tico</p></td>
-      <td><p>Referencia a la Norma ISO 9001:2008 7.1, 7.2.1, 7.5.1, 7.6, 8.1, 8.2.4</p></td>
-      <td></td>
-    </tr>
-    <tr>
+      <td rowspan='3'><?php
+            echo img('assets/images/logo-itc.png');
+          ?>
+      </td>
+      <td rowspan='2'><p>Nombre del formato: Formato para la planeaci&oacute;n del Curso y Avance Program&aacute;tico</p></td>
       <td><p>C&oacute;digo: SNEST-AC-PO-004-01</p></td>
+    </tr>
+    <tr>
       <td><p>Revisi&oacute;n: 5</p></td>
+    </tr>
+    <tr>
+      <td><p>Referencia a la Norma ISO 9001:2008 7.1, 7.2.1, 7.5.1, 7.6, 8.1, 8.2.4</p></td>
       <td><p>P&aacute;gina 1 de 3</p></td>
     </tr>
   </table>
