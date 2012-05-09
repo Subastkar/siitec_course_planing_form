39d3ce509428537e4833c4f67b49b11c8fa0bf64
Alberto Alcaraz <jaalcarazz@yahoo.com>
2012-05-08 21:58:13 -0500

    Changed query

--- application/models/materia_model.php
+++ application/models/materia_model.php
@@ -2,7 +2,7 @@
 
 class Materia_model extends CI_Model {
   function getCourse($materia){
-    $query = $this->db->query('select M.idmateria "id_materia", M.nombre "nombre_materia", M.horast "horas_teoricas", M.horasp "horas_practicas", M.creditos "creditos", M.numunidades "unidades", C.nombrecorto "carrera", C.idcarrera "id_carrera", A.nombre "aula", A.iddepartamento "id_departamento" from materia M, carrera C, aula A where M.idmateria = "' . $materia . '" group by M.idmateria');
+    $query = $this->db->query('select	M.idmateria "id_materia", M.nombre "nombre_materia", M.horast "horas_teoricas", M.horasp "horas_practicas", M.creditos "creditos", M.numunidades "unidades", C.nombrecorto "carrera", C.idcarrera "id_carrera", A.nombre "aula", A.iddepartamento "id_departamento", G.idgrupo "grupo" from	materia M, carrera C, aula A, grupo G where M.idmateria = "' . $materia . '" AND G.idmateria = "' . $materia . '" AND G.idcarrera = C.idcarrera group by M.idmateria');
     return $query->row();
   }
 }
