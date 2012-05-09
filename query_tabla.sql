ab623b9d55a8aeb6ec8c114d1361e6b7bbac8cc8
Alberto Alcaraz <jaalcarazz@yahoo.com>
2012-05-08 22:25:27 -0500

    Final query

--- query_tabla.sql
+++ query_tabla.sql
@@ -1,13 +1,19 @@
 select 
-	M.idmateria "id_materia", M.nombre "nombre_materia", M.horast "horas_teoricas", M.horasp "horas_practicas", M.creditos "creditos", M.numunidades "unidades", 
-	C.nombrecorto "carrera", C.idcarrera "id_carrera",
-	A.nombre "aula", A.iddepartamento "id_departamento"
+	M.idmateria "id_materia", M.nombre "nombre_materia", M.horast "horas_teoricas", M.horasp "horas_practicas", M.creditos "creditos", M.numunidades "unidades", C.nombrecorto "carrera", C.idcarrera "id_carrera", A.nombre "aula", A.iddepartamento "id_departamento", G.idgrupo "grupo", H.horain "hora_inicio", H.horafin "hora_fin"
 from 
 	materia M, 
 	carrera C, 
-	aula A
+	aula A,
+        grupo G,
+        horario H,
+        horariogrupo HG
 where 
-	M.idmateria = "TWM-0703" 
+	M.idmateria = "TWM-0703" AND
+        G.idmateria = "TWM-0703" AND
+        G.idcarrera = C.idcarrera AND
+        HG.idmateria = "TWM-0703" AND
+        HG.idperiodo = "2121" AND
+        H.idhorario = HG.idhorario
 group by 
 	M.idmateria
 
